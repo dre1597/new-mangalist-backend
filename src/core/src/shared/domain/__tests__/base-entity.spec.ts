e045d228e81f521a9f9c4ebd69f4020a8ae3ccb0
André Pereira <andrepereira97@yahoo.com>
2023-02-19 16:32:19 -0300

    :white_check_mark: add base entity tests

---
+++ src/core/src/shared/domain/__tests__/base-entity.spec.ts
@@ -0,0 +1,54 @@
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest';
+import { BaseEntity } from '../base-entity';
+import { regexUUID4 } from './utils';
+
+const executeAfterTwoMinutes = (func) => {
+  setTimeout(func, 1000 * 2);
+};
+
+class StubBaseEntity extends BaseEntity {}
+
+describe('BaseEntity', () => {
+  beforeEach(() => {
+    vi.useFakeTimers();
+  });
+
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('should initialize its own fields', () => {
+    const entity = new StubBaseEntity();
+
+    expect(entity.id).toMatch(regexUUID4);
+    expect(entity.createdAt).toBeInstanceOf(Date);
+    expect(entity.updatedAt).toBeInstanceOf(Date);
+  });
+
+  it('should be possible to update updatedAt field', () => {
+    const entity = new StubBaseEntity();
+    const updatedAt = entity.updatedAt.toISOString();
+
+    executeAfterTwoMinutes(() => entity.updateUpdateAt());
+
+    vi.advanceTimersByTime(2000);
+
+    const newUpdatedAt = entity.updatedAt.toISOString();
+
+    expect(updatedAt).not.toEqual(newUpdatedAt);
+  });
+
+  it('should not update createdAt on update updatedAt method', () => {
+    const entity = new StubBaseEntity();
+    const createdAt = entity.createdAt.toISOString();
+
+    executeAfterTwoMinutes(() => entity.updateUpdateAt());
+    vi.advanceTimersByTime(1000);
+
+    entity.updateUpdateAt();
+
+    const newCreatedAt = entity.createdAt.toISOString();
+
+    expect(createdAt).toEqual(newCreatedAt);
+  });
+});
